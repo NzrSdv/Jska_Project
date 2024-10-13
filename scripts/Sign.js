8c24d840723aa4591850f87325e6d4905f3788dd
Nazar <nzrsdv17@gmail.com>
2024-10-13 12:19:33 +0500

    added password show button and styles for it

--- scripts/Sign.js
+++ scripts/Sign.js
@@ -1,5 +1,6 @@
 let inputs = document.querySelectorAll(".input");
-let signInBtn = document.querySelector(".signUp-btn");
+let signInBtn = document.querySelector(".signBtn");
+let showBtn = document.querySelector(".show-btn");
 const specialChar = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 const numbersChar = /\d/;
 const validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -31,42 +32,44 @@ if (JSON.parse(localStorage.getItem("users")) != undefined) {
   UM = new UserManager([], {});
 }
 signInBtn.addEventListener("click", () => {
-  let name = inputs[0].value;
-  let login = inputs[1].value;
-  let email = inputs[2].value;
-  let password = inputs[3].value;
-  if (name.trim() == "") {
-    inputs[0].previousElementSibling.textContent = "important field";
-    inputs[0].previousElementSibling.classList.add("error");
-  } else {
-    if (login.trim() == "") {
-      inputs[1].previousElementSibling.textContent = "important field";
-      inputs[1].previousElementSibling.classList.add("error");
+  if(signInBtn.textContent == "Sign in"){
+    let name = inputs[0].value;
+    let login = inputs[1].value;
+    let email = inputs[2].value;
+    let password = inputs[3].value;
+    if (name.trim() == "") {
+      inputs[0].previousElementSibling.textContent = "important field";
+      inputs[0].previousElementSibling.classList.add("error");
     } else {
-      if (email.trim() == "") {
-        inputs[2].previousElementSibling.textContent = "important field";
-        inputs[2].previousElementSibling.classList.add("error");
+      if (login.trim() == "") {
+        inputs[1].previousElementSibling.textContent = "important field";
+        inputs[1].previousElementSibling.classList.add("error");
       } else {
-        if (password.trim() == "") {
-          inputs[3].previousElementSibling.textContent = "important field";
-          inputs[3].previousElementSibling.classList.add("error");
+        if (email.trim() == "") {
+          inputs[2].previousElementSibling.textContent = "important field";
+          inputs[2].previousElementSibling.classList.add("error");
         } else {
-          if (specialChar.test(name) || numbersChar.test(name)) {
-            inputs[0].previousElementSibling.textContent = "invalid value";
-            inputs[0].previousElementSibling.classList.add("error");
+          if (password.trim() == "") {
+            inputs[3].previousElementSibling.textContent = "important field";
+            inputs[3].previousElementSibling.classList.add("error");
           } else {
-            if (specialChar.test(login)) {
-              inputs[1].previousElementSibling.textContent = "invalid value";
-              inputs[1].previousElementSibling.classList.add("error");
+            if (specialChar.test(name) || numbersChar.test(name)) {
+              inputs[0].previousElementSibling.textContent = "invalid value";
+              inputs[0].previousElementSibling.classList.add("error");
             } else {
-              if (email.match(validEmail)) {
-                inputs[2].previousElementSibling.textContent = "invalid value";
-                inputs[2].previousElementSibling.classList.add("error");
+              if (specialChar.test(login)) {
+                inputs[1].previousElementSibling.textContent = "invalid value";
+                inputs[1].previousElementSibling.classList.add("error");
               } else {
-                let user = new User(name, login, email, password);
-                UM.addUser(user);
-                UM.lastUser = user;
-                console.log(UM);
+                if (email.match(validEmail)) {
+                  inputs[2].previousElementSibling.textContent = "invalid value";
+                  inputs[2].previousElementSibling.classList.add("error");
+                } else {
+                  let user = new User(name, login, email, password);
+                  UM.addUser(user);
+                  UM.lastUser = user;
+                  console.log(UM);
+                }
               }
             }
           }
@@ -74,4 +77,19 @@ signInBtn.addEventListener("click", () => {
       }
     }
   }
+  else{
+
+  }
 });
+
+showBtn.addEventListener("click",() => {
+  let passwordInput = showBtn.previousElementSibling.lastElementChild;
+  if(passwordInput.type == "text"){
+    passwordInput.type = "password"
+    showBtn.firstElementChild.src = "../imgs/visible-ico.svg"
+  }
+  else{
+    passwordInput.type = "text";
+    showBtn.firstElementChild.src = "../imgs/not-visible-ico.svg"
+}
+})
